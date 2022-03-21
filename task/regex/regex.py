38d7b2ec24c8eccd115d14e31fb9a01e23da1cb1
MkMula <maho.mamulashvili@gmail.com>
2022-03-21 18:32:45 -0400

    Check each character of regex and string

--- task/regex/regex.py
+++ task/regex/regex.py
@@ -1,14 +1,32 @@
 from re import match
-# write your code here
-usr_inp = input()
 
-if not usr_inp:
-    print(True)
-elif len(usr_inp) < 3:
-    if usr_inp[0] == '|':
-        print(True)
-    elif usr_inp[1] == '|':
-        print(False)
-else:
-    result = bool(match(usr_inp[0], usr_inp[2]))
+
+def match_it(regex, string):
+    """
+    Recursion function. Returns True if every character in regular exspressiona
+    and in string matches.
+
+    :param regex: Regular expression. String on the right side of '|'
+    :param string:
+    :return: True or False
+    """
+    if not regex:
+        return True
+    elif not string:
+        return False
+    is_same = bool(match(regex[0], string[0]))
+
+    if is_same:
+        return match_it(regex[1:], string[1:])
+
+    return False
+
+
+def main():
+    regex, chars = input().split('|')
+    result = match_it(regex, chars)
     print(result)
+
+
+if __name__ == '__main__':
+    main()
