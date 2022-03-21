08bcd6aaff1f15732e90ab360370e3ffc3270fb1
MkMula <maho.mamulashvili@gmail.com>
2022-03-21 19:29:34 -0400

    Compare unequal strings
    If unequal regular expression and string passed, compare string to
    regexp by passing string with incremented starting position to equal
    matching function.

--- task/regex/regex.py
+++ task/regex/regex.py
@@ -1,31 +1,49 @@
 from re import match
 
 
-def match_it(regex, string):
+def match_it(regex, chars):
     """
-    Recursion function. Returns True if every character in regular exspressiona
-    and in string matches.
+    Recursion function. Returns True if every character in regular expression
+    and in string match.
 
-    :param regex: Regular expression. String on the right side of '|'
-    :param string:
+    :param regex: Regular expression.
+    :param chars: String to match the regex
     :return: True or False
     """
     if not regex:
         return True
-    elif not string:
+    elif not chars:
         return False
-    is_same = bool(match(regex[0], string[0]))
+    is_same = bool(match(regex[0], chars[0]))
 
     if is_same:
-        return match_it(regex[1:], string[1:])
+        return match_it(regex[1:], chars[1:])
 
     return False
 
 
+def slice_string(regex, chars):
+    """
+    Recursion function. Calls itself until length of the string is 0. or
+    returns True if Regexp and String match.
+
+    :param regex: Regular expression.
+    :param chars: String to match the regex
+    :return: True or False
+    """
+    if not regex:  # In case if empty regexp
+        return True
+    elif not chars:
+        return False
+    elif match_it(regex, chars):
+        return True
+
+    return slice_string(regex, chars[1:])
+
+
 def main():
     regex, chars = input().split('|')
-    result = match_it(regex, chars)
-    print(result)
+    print(slice_string(regex, chars))
 
 
 if __name__ == '__main__':
